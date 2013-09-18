d1c430813b563a862501f6b0eecb98b605f3447e
vanesagh <vanesag@chalmers.se>
2013-09-18 21:50:30 +0200

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,23 +1,34 @@
 require 'spec_helper'
 
 describe "Static pages" do
-
+subject {page}
   describe "Home page" do
+  before{visit root_path}
+ it { should have_content('New App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
 
-    it "should have the content 'New App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('New App')
-    end
+  end
 
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
-    end
+  describe "Help page" do
+    before { visit help_path }
 
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('')) }
   end
   
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_content('About') }
+    it { should have_title(full_title('')) }
+  end
+  
+
+   describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('')) }
+  end
 end

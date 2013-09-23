0adf605010add559dd7c75f6f5e5e8fec3fe728c
vanesagh <vanesag@chalmers.se>
2013-09-23 18:16:31 +0200

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,8 @@ class UsersController < ApplicationController
    def create
     @user = User.new(user_params)
     if @user.save
-    flash[:success] = "Welcome to the Sample App!"
+    sign_in @user
+    flash[:success] = "Welcome to the Real time communication App!"
     redirect_to @user
     else
       render 'new'
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,5 +1,5 @@
 def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
+  base_title = "Real time communication App"
   if page_title.empty?
     base_title
   else

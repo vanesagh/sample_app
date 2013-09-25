68481e3398dd192f3ae7e00f85b92ff1e5b32d9f
vanesagh <vanesag@chalmers.se>
2013-09-25 22:06:43 +0200

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -98,7 +98,28 @@ describe "authorization" do
           it { should have_title('Sign in') }
         end 
         
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end       
       end # end of in the users controller
+      
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
     end # end of for non-signed users
     
     describe "as wrong user" do
